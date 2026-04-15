3a00decfe1cd6959433426df3806d094a7129df8
SajibSaha207 <sajibsaha.207@gmail.com>
2026-04-15 11:18:12 +0600

    reverse string  done

--- Day1.py
+++ Day1.py
@@ -1,13 +1,36 @@
+# #PROBLEM: TWO SUM(FOR TARGET VALUE)
+# numbers = [2,7,11,15]
+# target = 9
+# for i in range(len(numbers)):
+#                 for j in range(i + 1, len(numbers)):
+#                     if numbers [i] + numbers [j] == target:
+#                            print(i,j)
+            
+                
+                    
+
+# #Find dublicate
+# nums =[1,2,5,1]
+
+# seen = set()
+# for num in nums:
+#     if num in seen:
+#         print ("True")
+#         seen.add(num)
+# print("False")
 
 
-#THIRD MAXMUM NUMBER
-nums = [3,2,1]
-nums = set(nums)
+# #THIRD MAXMUM NUMBER
+# nums = [3,2,1]
+# nums = set(nums)
 
-if len(nums)< 3:
-    print(max(nums))
-else:
-    nums= sorted(nums, reverse=True)
-print(nums[2])  
-
-  
+# if len(nums)< 3:
+#     print(max(nums))
+# else:
+#     nums= sorted(nums, reverse=True)
+# print(nums[2])  
+    
+#REVERSE STRING
+s=["h","e","l","l","o"]
+reversed_str = s[::-1]
+print(reversed_str)  # Output: dlrow olleH
