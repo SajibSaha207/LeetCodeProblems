1b5ea8fa93c2cbf9f5636d4a9c9889a02602badd
SajibSaha207 <sajibsaha.207@gmail.com>
2026-04-15 08:13:19 +0600

    sum of two(find target value) done

--- Day1.py
+++ Day1.py
@@ -1,5 +1,13 @@
 #PROBLEM: TWO SUM(FOR TARGET VALUE)
-
+numbers = [2,7,11,15]
+target = 9
+for i in range(len(numbers)):
+                for j in range(i + 1, len(numbers)):
+                    if numbers [i] + numbers [j] == target:
+                           print(i,j)
+            
+                
+                    
 
 #Find dublicate
 nums =[1,2,5,1]
